539cde5b2503de85fc3857e01624ffefc8200215
marsh <891871898@qq.com>
2020-11-22 12:14:49 +0000

    chore: cargo fmt

--- pallets/advanca-core/src/mock.rs
+++ pallets/advanca-core/src/mock.rs
@@ -16,9 +16,9 @@
 // Creating mock runtime here
 
 use crate::*;
-use pallet_balances;
 use frame_support::{impl_outer_event, impl_outer_origin, parameter_types, weights::Weight};
 use frame_system as system;
+use pallet_balances;
 use sp_core::H256;
 use sp_runtime::{
     testing::Header,
