bd2c71d249ee639cbd12134c34d90a9c1a140a45
marsh <891871898@qq.com>
2020-11-22 12:49:45 +0000

    fix: install.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,4 +9,5 @@ if [ -z $CI_PROJECT_NAME ] ; then
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup install nightly-2020-10-06
+rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
